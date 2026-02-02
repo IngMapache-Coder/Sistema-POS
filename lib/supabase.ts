e77be09168510d79e3374c6dc1422b2210cb7b1b
Daniel Lozano <Daniel.Lozano@Jala.University>
2026-02-02 02:53:28 -0500

    xd

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://sdyqmpcmbrtwibuqtzlc.supabase.co';
-const supabaseAnonKey = 'sb_publishable_KGUXRer8tdSzLcY9oulJng_S30YuLm4';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
